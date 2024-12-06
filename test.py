3cd4973ab41f1c0e683af44f4dbf9fde5fad3bf3
Sikander Azam <sikanderazam276@gmail.com>
2024-12-07 01:13:53 +0500

    adding test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print(13/10)
+# s = [1, 3,5,6,7]
+
+# for index, chr in enumerate(s):
+#     print(f"ind: {index} , chr: {chr}")
+
+
+dictionary = {}
+
+# dictionary['b'] = 4
+
+
+print(dictionary)
+
+dictionary['b'] = 6 + dictionary.get('b', 0)
+
+print(dictionary)
